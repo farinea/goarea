b058a1a34aafbab1e90f1f15a7470d134fdfcbba
farinea <47990866+farinea@users.noreply.github.com>
2019-11-01 18:53:51 -0300

    Criação do arquivo pacotearea.go

---
+++ pacotearea.go
@@ -0,0 +1,22 @@
+package pacotearea
+
+import "math"
+
+// Pi proporção numérica definida pela relação entre o perimetro de uma circunferência e seu diâmetro
+const Pi = 3.1416
+
+// Circ responsável por calcular a área da circunferência
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * Pi
+}
+
+// Rect responsável por calcular a área de um retângulo
+func Rect(base, altura float64) float64 {
+	return base * altura
+}
+
+// Não visível
+func _TrianguloEq(base, altura float64) float64 {
+	return (base * altura) / 2
+
+}
